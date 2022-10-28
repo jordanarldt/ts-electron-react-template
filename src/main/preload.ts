e3ddac14104152323f8b1fcb13d98c11c815b3d1
Jordan Arldt <jordan.arldt@bigcommerce.com>
2022-10-28 13:50:03 -0500

    Remove unneeded code in preload.ts

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -8,19 +8,3 @@ import { contextBridge, ipcRenderer } from "electron";
 contextBridge.exposeInMainWorld("app", {
   exampleFunction: () => ipcRenderer.sendSync("example-function"),
 });
-
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector: string, text: string) => {
-    const element = document.getElementById(selector);
-    if (element) {
-      element.innerText = text;
-    }
-  };
-
-  for (const type of ["chrome", "node", "electron"]) {
-    replaceText(
-      `${type}-version`,
-      process.versions[type as keyof NodeJS.ProcessVersions],
-    );
-  }
-});
